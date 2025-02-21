5956c126b8659bb988e66b10f1f9ccb4fad4de18
JiahanDu <jiahandu@math.ucla.edu>
2025-02-21 12:17:44 -0500

    Update linear_solver.py

--- linear_solver.py
+++ linear_solver.py
@@ -15,6 +15,7 @@ def search(A:np.ndarray,b:np.ndarray,c:np.ndarray,d:int):
     try:
         (max_,values)=solve(A,b,c)
     except:
+        print(A,b,c,"failed")
         return
     if max_<=cur_max:
         pass
@@ -44,8 +45,25 @@ def main(A:list[list[int]],b:list[int],c:list[int],d:int):
     global cur_max
     cur_max=0
     cur_max=-float('inf')
+    print(solve(A,b,c))
     search(A,b,c,d)
     return cur_max
 
 #Test case:
-print(main([[-1,1],[3,2],[2,3]],[1,12,12],[0,1],2))
+# print(main([[-1,1],[3,2],[2,3]],[1,12,12],[0,1],2))
+#Teset case
+print(main([[3,2,1],[2,3,4],[5,1,0],[-1,2,-2]],[90,100,52,-1],[4,5,1],2))
+import gurobipy as grb
+# model=grb.Model("test")
+# x=model.addVar(vtype=grb.GRB.INTEGER,name="x")
+# y=model.addVar(vtype=grb.GRB.INTEGER,name="y")
+# z=model.addVar(name="z")
+# model.addConstr(3*x+2*y+z<=90)
+# model.addConstr(2*x+3*y+4*z<=100)
+# model.addConstr(5*x+y<=52)
+# model.addConstr(-x+2*y-2*z<=-1)
+# model.addConstr(x>=0)
+# model.addConstr(y>=0)
+# model.addConstr(z>=0)
+# model.setObjective(4*x+5*y+z,grb.GRB.MAXIMIZE)
+# model.optimize() 

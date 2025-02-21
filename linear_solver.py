b3b289e91165be3200d50718f051c0898c34e5d5
JiahanDu <jiahandu@math.ucla.edu>
2025-02-21 15:37:45 -0500

    committ

--- linear_solver.py
+++ linear_solver.py
@@ -1,7 +1,8 @@
 from solve import solve
 import numpy as np
-
+import random
 def isSolution(x:np.ndarray,d:int):
+    print(x)
     for i in range(d):
         if not float.is_integer(x[i]):
             return False
@@ -10,12 +11,14 @@ def isSolution(x:np.ndarray,d:int):
 cur_max=-float('inf')
 
 def search(A:np.ndarray,b:np.ndarray,c:np.ndarray,d:int):
+    A_=np.copy(A)
+    b_=np.copy(b)
+    c_=np.copy(c)
     global cur_max
     #First d variables are integer variables, the rest are continuous variables.
     try:
         (max_,values)=solve(A,b,c)
     except:
-        print(A,b,c,"failed")
         return
     if max_<=cur_max:
         pass
@@ -28,13 +31,13 @@ def search(A:np.ndarray,b:np.ndarray,c:np.ndarray,d:int):
         x=np.zeros(len(A[0]),dtype=np.float64)
         x[i]=1
         try:
-            search(np.append(A,[x],axis=0),np.append(b,float(values[i]//1)),c,d)
+            search(np.append(A_,[x],axis=0),np.append(b_,float(values[i]//1)),c_,d)
         except:
             pass
         y=np.zeros(len(A[0]),dtype=np.float64)
         y[i]=-1
         try:
-            search(np.append(A,[y],axis=0),np.append(b,-float(values[i]//1+1)),c,d)
+            search(np.append(A_,[y],axis=0),np.append(b_,-float(values[i]//1+1)),c_,d)
         except:
             pass
 
@@ -45,25 +48,19 @@ def main(A:list[list[int]],b:list[int],c:list[int],d:int):
     global cur_max
     cur_max=0
     cur_max=-float('inf')
-    print(solve(A,b,c))
     search(A,b,c,d)
     return cur_max
 
-#Test case:
-# print(main([[-1,1],[3,2],[2,3]],[1,12,12],[0,1],2))
-#Teset case
-print(main([[3,2,1],[2,3,4],[5,1,0],[-1,2,-2]],[90,100,52,-1],[4,5,1],2))
+A=[]
+for i in range(1000):
+    A.append([])
+    for j in range(1000):
+        A[i].append(10*random.random())
+b=[]
+for i in range(1000):
+    b.append(1000000*random.random())
+c=[]
+for i in range(1000):
+    c.append(random.random())
+print(main(A,b,c,5))
 import gurobipy as grb
-# model=grb.Model("test")
-# x=model.addVar(vtype=grb.GRB.INTEGER,name="x")
-# y=model.addVar(vtype=grb.GRB.INTEGER,name="y")
-# z=model.addVar(name="z")
-# model.addConstr(3*x+2*y+z<=90)
-# model.addConstr(2*x+3*y+4*z<=100)
-# model.addConstr(5*x+y<=52)
-# model.addConstr(-x+2*y-2*z<=-1)
-# model.addConstr(x>=0)
-# model.addConstr(y>=0)
-# model.addConstr(z>=0)
-# model.setObjective(4*x+5*y+z,grb.GRB.MAXIMIZE)
-# model.optimize() 

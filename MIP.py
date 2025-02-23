abac2b139e76a07c30ee3b8e2c17705e1a6ad279
JiahanDu <jiahandu@math.ucla.edu>
2025-02-23 15:15:11 -0500

    Update MIP.py

--- MIP.py
+++ MIP.py
@@ -34,9 +34,12 @@ class Model:
             self.A.append(x)
             self.b.append(bound)
         elif type=='>=':
-            pass
+            for key in equation:
+                equation[key]*=(-1)
+                self.addConstr(equation,-bound,'<=')
         elif type=='=':
-            pass
+            self.addConstr(equation,bound,'<=')
+            self.addConstr(equation,bound,'>=')
 
     def setObjective(self,equation):
         x=[0]*self.num
@@ -47,91 +50,3 @@ class Model:
     def optimize(self):
         result=solver_matrix(self.A,self.b,self.c,self.d)
         print(result)
-
-########################################################
-########################################################
-n = 2  # Number of tasks
-M = 2000  # Large constant for Big M method
-for k in range(1,n):
-    # Task times
-    t = [4, 3]
-
-    # Waiting time matrix
-    A = [[1, 4],
-        [3, 2]]
-    
-
-    # Create model
-    model = Model()
-
-    # Decision variables
-    x = {}  # Dictionary to store variables
-    for i in range(n):
-        for j in range(n):
-            if i != j:
-                model.addVar('binary',f"x_{i}_{j}")
-    
-    # for i in range(n):
-    #     model.addVar('continuous', f'y_{i}')  # Start times
-    #     model.addVar('continuous', f'z_{i}')  # Finish times
-    
-    # # Constraints: Each task appears once in sequence
-    for i in range(n):
-        if i!=k:
-            equation={}
-            for j in range(n):
-                if j!=i:
-                    equation[f'x_{i}_{j}']=1
-            model.addConstr(equation,1)
-            equation={}
-            for j in range(n):
-                if j!=i:
-                    equation[f'x_{i}_{j}']=-1
-            model.addConstr(equation,-1)
-        if i!=0:
-            equation={}
-            for j in range(n):
-                if j!=i:
-                    equation[f'x_{j}_{i}']=1
-            model.addConstr(equation,1)
-            equation={}
-            for j in range(n):
-                if j!=i:
-                    equation[f'x_{j}_{i}']=-1
-            model.addConstr(equation,-1)
-
-    # for i in range(n):
-    #     if i!=0:
-    #         model.addConstr({f'x_{i}_0':1},0)
-    #         model.addConstr({f'x_{i}_0':-1},0)
-    #     if i!=k:
-    #         model.addConstr({f'x_{k}_{i}':1},0)
-    #         model.addConstr({f'x_{k}_{i}':-1},0)
-
-    # Task timing constraints
-    # for i in range(n):
-    #     for j in range(n):
-    #         if i != j:
-    #             equation={f'y_{j}':-1,f'z_{i}':1,f'x_{i}_{j}':(A[i][j]+M)}
-    #             model.addConstr(equation,M)
-    #             equation={f'y_{j}':1,f'z_{i}':-1,f'x_{i}_{j}':-(A[i][j]-M)}
-    #             model.addConstr(equation,M)
-
-    # Task duration constraints
-    # for i in range(n):
-    #     model.addConstr({f'z_{i}':1,f'y_{i}':-1},t[i])
-    #     model.addConstr({f'z_{i}':-1,f'y_{i}':1},-t[i])
-
-    # for i in range(n):
-    #     model.addConstr({f'y_{i}':1},100)
-    # Objective function: minimize total cost
-    equation={}
-    for i in range(n):
-        for j in range(n):
-            if i!=j:
-                equation[f'x_{i}_{j}']=-A[i][j]
-    model.setObjective(equation)
-    print(model.l)
-    print(model.A,model.b,model.c,model.d)
-    # Optimize model
-    model.optimize()
